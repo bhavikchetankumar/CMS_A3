9354c39d5d6df9db81adcb021dc2b10b32edacef
bhavikchetankumar <45928871+bhavikchetankumar@users.noreply.github.com>
2019-10-01 16:26:03 +1000

    Adding files
    Second commit

---
+++ CustomPlugIn_A3/CustomPlugIn_A3.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+Plugin Name: CustomPlugIn_A3
+Plugin URI: https://www.abc.com
+
+Description: A Plugin created as a part of CMS Assignment 3
+Version: 1.0
+Author: Bhavik Shah <www.facebook.com/BhavikShah746>
+Author URI: https://www.facebook.com/BhavikShah746
+
+License: No License
+*/
+
+?>
